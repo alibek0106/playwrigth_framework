cdb184a16f1e81e44abd6c7ff6fcbe747e806412
Alibek Abdykarimov <a.abdykarimov@a1qa.com>
2025-12-01 18:44:34 +0500

    feat(api): add task assignment tests and dependencies

---
+++ src/utils/DataFactory.ts
@@ -0,0 +1,43 @@
+import { faker } from '@faker-js/faker';
+import { CreateUserRequest } from '../models/api/ReqResModels';
+
+export class DataFactory {
+    /**
+     * Generate a repeatable (deterministic) list of users.
+     * Useful for Data Driven Tests where we want consistent results in CI 
+     */
+    static generateDeterministicUserList(count: number, seed: number = 123): CreateUserRequest[] {
+        faker.seed(seed);
+        return this.generateUserList(count);
+    }
+    /**
+     * Generates an array of random users for DDT.
+     * @param count - How many users to generate
+     */
+    static generateUserList(count: number): CreateUserRequest[] {
+        const users: CreateUserRequest[] = [];
+
+        for (let i = 0; i < count; i++) {
+            users.push({
+                name: faker.person.fullName(),
+                job: faker.person.jobTitle()
+            });
+        }
+
+        // Edge Case
+        users.push({
+            name: "OConner-Smith",
+            job: "QA & Automation"
+        });
+
+        return users;
+    }
+
+    static getCheckoutDetails() {
+        return {
+            firstName: faker.person.firstName(),
+            lastName: faker.person.lastName(),
+            postalCode: faker.location.zipCode()
+        };
+    }
+}
