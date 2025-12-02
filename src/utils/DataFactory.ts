5da30612be76a845254beb6d5283e942bc68693c
Alibek Abdykarimov <a.abdykarimov@a1qa.com>
2025-12-02 11:21:58 +0500

    updated DataFactory and fixed Routes

--- src/utils/DataFactory.ts
+++ src/utils/DataFactory.ts
@@ -13,24 +13,36 @@ export class DataFactory {
     /**
      * Generates an array of random users for DDT.
      * @param count - How many users to generate
+     * @param includeEdgeCases - Whether to include edge cases
      */
-    static generateUserList(count: number): CreateUserRequest[] {
+    static generateUserList(count: number, includeEdgeCases: boolean = false): CreateUserRequest[] {
+
+        if (includeEdgeCases && count < 1) {
+            throw new Error('Count must be at least 1 to include edge cases');
+        }
+
         const users: CreateUserRequest[] = [];
 
-        for (let i = 0; i < count; i++) {
+        const randomCount = includeEdgeCases ? count - 1 : count;
+
+        for (let i = 0; i < randomCount; i++) {
             users.push({
                 name: faker.person.fullName(),
                 job: faker.person.jobTitle()
             });
         }
 
-        // Edge Case
-        users.push({
-            name: "OConner-Smith",
-            job: "QA & Automation"
-        });
-
+        if (includeEdgeCases) {
+            users.push({
+                name: "OConner-Smith",
+                job: "QA & Automation"
+            });
+        }
         return users;
+    }
+
+    static getEdgeCaseUser() {
+
     }
 
     static getCheckoutDetails() {
@@ -38,6 +50,15 @@ export class DataFactory {
             firstName: faker.person.firstName(),
             lastName: faker.person.lastName(),
             postalCode: faker.location.zipCode()
+        };
+    }
+
+    static getKnownUser() {
+        return {
+            id: 2,
+            email: 'janet.weaver@reqres.in',
+            firstName: 'Janet',
+            lastName: 'Weaver'
         };
     }
 }
